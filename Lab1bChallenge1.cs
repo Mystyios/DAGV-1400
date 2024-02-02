7d6ba8befc576a9383effeb95b950b2f616ca015
Mystyios <156443472+Mystyios@users.noreply.github.com>
2024-02-02 12:41:23 -0700

    Updated Lab1bChallenge1.cs
    Extended the code to include additional responses based on different temperature ranges

--- Lab1bChallenge1.cs
+++ Lab1bChallenge1.cs
@@ -9,14 +9,31 @@ public class Program
         int temp = Convert.ToInt32(Console.ReadLine());
 
       //Determines what message to respond with depending on if the temperature is greater or less than 30.
-        if (temp > 30)
+        if (temp < 0)
         {
-            Console.WriteLine("You should stay hydrated and avoid too much sunlight");
+            Console.WriteLine("It's freezing, stay inside and keep warm!");
         }
-        else
+        else if (temp >= 1 && temp <= 10)
         {
-            Console.WriteLine("It is a nice day, you should enjoy the weather");
+            Console.WriteLine("It's a bit chilly, I'd reccomend a coat");
         }
+		else if (temp >= 11 && temp <= 20)
+		{
+			Console.WriteLine("It's cool outside, you should take a light jacket");
+		}
+		else if (temp >= 21 && temp <= 30)
+		{
+			Console.WriteLine("It's a nice warm day, enjoy the weather");
+		}
+		else if (temp >= 31 && temp <= 40)
+		{
+			Console.WriteLine("It's pretty hot outside, stay hydrated and wear sunscreen");
+		}
+		else
+		{
+			Console.WriteLine("It is absurdly hot, drink plenty of water and avoid the sun!");
+		}
+		
         
     }
 }
